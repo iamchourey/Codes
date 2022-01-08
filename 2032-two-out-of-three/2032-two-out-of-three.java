6a588947aad7eb2a587a4bfba17f1c403a5c988e
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-01-08 13:46:03 +0530

    Time: 4 ms (70.25%), Space: 43.1 MB (21.61%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.java
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -1,35 +1,19 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         
-        int n1=nums1.length,n2=nums2.length,n3=nums3.length;
+        int arr[]=new int[101];
         
-        Map<Integer,Integer> hm=new HashMap<>();
+        for(int num:nums1)
+            arr[num]=1;
+        for(int num:nums2)
+            arr[num]=arr[num]|(1<<1);
+        for(int num:nums3)
+            arr[num]=arr[num]|(1<<2);
         
-        for(int i=0;i<n1;i++){
-            hm.put(nums1[i],1);
-        }
-        
-        Arrays.sort(nums2);
-        for(int i=0;i<n2;i++){
-            
-            if(i>0 && nums2[i]==nums2[i-1])
-                continue;
-            
-            hm.put(nums2[i],hm.getOrDefault(nums2[i],0)+1);
-        }
-        Arrays.sort(nums3);
-        for(int i=0;i<n3;i++){
-            
-            if(i>0 && nums3[i]==nums3[i-1])
-                continue;
-            
-            hm.put(nums3[i],hm.getOrDefault(nums3[i],0)+1);
-        }
         List<Integer> ls=new ArrayList<>();
-        
-        for(Map.Entry<Integer,Integer> entry:hm.entrySet()){
-            if(entry.getValue()>1)
-                ls.add(entry.getKey());
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==3 || arr[i]==5 || arr[i]==6 || arr[i]==7)
+                ls.add(i);
         }
         
         return ls;
