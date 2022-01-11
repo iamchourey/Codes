d45f6900b18563983f06fb614db4ac674ff73008
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-01-11 06:53:29 +0530

    Time: 1 ms (50.87%), Space: 39 MB (41.54%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -16,40 +16,19 @@
 class Solution {
     public int sumRootToLeaf(TreeNode root) {
         
-        List<String> ls=new ArrayList<>();
-        helper(root,"",ls);
-        
-        int sum=0;
-        
-        for(int i=0;i<ls.size();i++){
-            String s=ls.get(i);
-            int num=0;
-            int base=1;
-            for(int j=s.length()-1;j>=0;j--){
-                num=num+(s.charAt(j)-'0')*base;
-                base=base*2;
-            }
-            
-            sum+=num;
-        }
-        return sum;
-        
+        return sumMethod(root,0);
     }
     
-    public void helper(TreeNode root,String str,List<String> ls){
+    public int sumMethod(TreeNode root,int sum){
         
-        if(root==null){
-            return;
-        }
+        if(root==null)
+            return 0;
+        
+        sum=sum*2+root.val;
         
         if(root.left==null && root.right==null)
-        {
-            str=str+root.val;
-            ls.add(str);
-        }
-        
-        str=str+root.val;
-        helper(root.left,new String(str),ls);
-        helper(root.right,new String(str),ls);
+            return sum;
+        else
+            return sumMethod(root.left,sum)+sumMethod(root.right,sum);
     }
 }
