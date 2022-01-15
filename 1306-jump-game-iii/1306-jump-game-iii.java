e5ecd5b6e0cc1c8b297e9cfa5d24b2ba64e7d4ef
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-01-15 09:17:37 +0530

    Time: 3 ms (64.70%), Space: 56.2 MB (21.53%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,34 +1,43 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
         
-        Queue<Integer> q=new LinkedList<>();
-        q.add(start);
+//         Queue<Integer> q=new LinkedList<>();
+//         q.add(start);
         
-        while(q.isEmpty()==false){
+//         while(q.isEmpty()==false){
             
-            int size=q.size();
+//             int size=q.size();
             
-            while(size-->0){
-                int point=q.poll();
+//             while(size-->0){
+//                 int point=q.poll();
                 
-                if(point-arr[point] >=0){
-                    if(arr[point-arr[point]]==0)
-                        return true;
-                    else if(arr[point-arr[point]]>0)
-                        q.add(point-arr[point]);
-                }
+//                 if(point-arr[point] >=0){
+//                     if(arr[point-arr[point]]==0)
+//                         return true;
+//                     else if(arr[point-arr[point]]>0)
+//                         q.add(point-arr[point]);
+//                 }
                 
-                if(point+arr[point]<arr.length){
-                    if(arr[point+arr[point]]==0)
-                        return true;
-                    else if(arr[point+arr[point]]>0)
-                        q.add(point+arr[point]);
-                }
+//                 if(point+arr[point]<arr.length){
+//                     if(arr[point+arr[point]]==0)
+//                         return true;
+//                     else if(arr[point+arr[point]]>0)
+//                         q.add(point+arr[point]);
+//                 }
                 
-                arr[point]=-arr[point];
-            }
-        }
+//                 arr[point]=-arr[point];
+//             }
+//         }
         
-        return false;
+//         return false;
+        
+        if(start<0 || start>=arr.length || arr[start]<0)
+            return false;
+        if(arr[start]==0)
+            return true;
+        
+        arr[start]=-arr[start];
+        
+        return canReach(arr,start+arr[start])||canReach(arr,start-arr[start]);
     }
 }
