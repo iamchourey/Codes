4c870afd8c51ea9a90dc4ca5144c949f7d983e94
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-01-22 12:37:05 +0530

    Time: 26 ms (52.83%), Space: 114.1 MB (46.56%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,37 +1,16 @@
 class Solution {
     
-    public int helper(int[] stoneValue,int i,int j,int dp[]){
-        
-        if(i>j)
-            return 0;
-        
-        if(dp[i]!=-1)
-            return dp[i];
-        
-        int ans=Integer.MIN_VALUE;
-        
-        ans=Math.max(ans,stoneValue[i]-helper(stoneValue,i+1,j,dp));
-        if((i+1)<=j)
-            ans=Math.max(ans,stoneValue[i]+stoneValue[i+1]-helper(stoneValue,i+2,j,dp));
-        if((i+2)<=j)
-            ans=Math.max(ans,stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-helper(stoneValue,i+3,j,dp));
-        
-        return dp[i]=ans;
-    }
-    public String stoneGameIII(int[] stoneValue) {
-        
-        int n=stoneValue.length-1;
-        
-        int[] dp=new int[n+1];
-        
-        for(int i=0;i<=n;i++){
-            dp[i]=-1;
+     public String stoneGameIII(int[] A) {
+        int n = A.length, dp[] = new int[4];
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i % 4] = Integer.MIN_VALUE;
+            for (int k = 0, take = 0; k < 3 && i + k < n; ++k) {
+                take += A[i + k];
+                dp[i % 4] = Math.max(dp[i % 4], take - dp[(i + k + 1) % 4]);
+            }
         }
-        if(helper(stoneValue,0,n,dp)==0)
-            return "Tie";
-        else if(helper(stoneValue,0,n,dp)>0)
-            return "Alice";
-        else
-            return "Bob";
+        if (dp[0] > 0) return "Alice";
+        if (dp[0] < 0) return "Bob";
+        return "Tie";
     }
 }
