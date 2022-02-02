16f9306ef5fa5d12d96a2ed5b1d249f8664a2eae
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-02-02 10:08:27 +0530

    Time: 13 ms (68.73%), Space: 47.6 MB (17.82%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    public boolean compare(int[] freq1,int[] freq2){
+        
+        for(int i=0;i<26;i++){
+            if(freq1[i]!=freq2[i])
+                return false;
+        }
+        
+        return true;
+    }
+    public List<Integer> findAnagrams(String s, String p) {
+        
+        
+        List<Integer> ls=new ArrayList<>();
+        if(s.length()<p.length())
+            return ls;
+        
+        int[] freq=new int[26];
+        for(char c:p.toCharArray()){
+            freq[c-'a']++;
+        }
+        
+        int window=p.length();
+        int freq2[]=new int[26];
+        
+        for(int i=0;i<window;i++){
+            freq2[s.charAt(i)-'a']++;
+        }
+        for(int i=window;i<s.length();i++){
+            
+            if(compare(freq,freq2))
+                ls.add(i-window);
+            
+            freq2[s.charAt(i)-'a']++;
+            freq2[s.charAt(i-window)-'a']--;
+        }
+        
+        if(compare(freq,freq2))
+            ls.add(s.length()-window);
+        
+        return ls;
+    }
+}
