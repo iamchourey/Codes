9ac4f69e9c268d9658a8420437a03afbf9108bda
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-02-08 15:10:08 +0530

    Time: 2 ms (59.15%), Space: 41.3 MB (9.91%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int addDigits(int num) {
+        
+        if(num<10)
+            return num;
+        else
+            return addDigits(num%10 + addDigits(num/10));
+    }
+}
