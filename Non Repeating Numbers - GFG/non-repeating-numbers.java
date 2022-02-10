d0db6d0ca860f6c2292934cbda2d7c2f35722255
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-02-10 15:52:25 +0530

    Added solution - LeetHub

---
+++ Non Repeating Numbers - GFG/non-repeating-numbers.java
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v = new int[2 * n + 2];
+            for(int i = 0; i < 2 * n + 2; i++)
+            {
+                v[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = ob.singleNumber(v);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] singleNumber(int[] nums)
+    {
+        // Code here
+        int xor=0;
+        
+        for(int num:nums){
+            xor^=num;
+        }
+        
+        int mask=xor & -xor;
+        
+        int[] res=new int[2];
+        
+        for(int num:nums){
+            if((num&mask) == 0)
+            res[1]^=num;
+            else
+            res[0]^=num;
+        }
+        
+        Arrays.sort(res);
+        
+        return res;
+    }
+}
