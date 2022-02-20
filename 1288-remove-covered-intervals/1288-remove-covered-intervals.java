1266fb43318d50e25ffcc086fdbe56847f615c14
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-02-20 12:10:45 +0530

    Time: 6 ms (71.14%), Space: 46 MB (26.37%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -3,22 +3,19 @@ class Solution {
         
         Arrays.sort(intervals,new Comparator<int[]>(){
             public int compare(int[] a,int[] b){
-                if(a[1]==b[1])
-                    return b[0]-a[0];
-                return a[1]-b[1];
+                return a[0]-b[0];
             }
         });
         
-        int n=intervals.length;
-        int res=n;
-        int end=intervals[n-1][1],start=intervals[n-1][0];
-        for(int i=n-2;i>=0;i--){
+        int start=-1,end=-1,res=0;
+        
+        for(int[] a:intervals){
+            if(a[0]>start && a[1]>end){
+                start=a[0];
+                res++;
+            }
             
-            if(intervals[i][0]>=start && intervals[i][1]<=end)
-                res--;
-            
-            end=Math.max(end,intervals[i][1]);
-            start=Math.min(start,intervals[i][0]);
+            end=Math.max(a[1],end);
         }
         
         return res;
