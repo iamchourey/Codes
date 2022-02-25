1e595b9f9bec6e84c70e81fb2579d1d1c99346ce
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-02-25 08:44:13 +0530

    Time: 1 ms (90.83%), Space: 42.3 MB (15.33%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -1,61 +1,19 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
         
-        ArrayList<Integer> ls1=new ArrayList<>();
-        ArrayList<Integer> ls2=new ArrayList<>();
-        
-        for(int i=0;i<version1.length();){
-            
-            if(version1.charAt(i)=='.'){
-                i++;
-                continue;
-            }
-            
-            int version=0;
-            while(i<version1.length() && version1.charAt(i)!='.'){
-                version=version*10+version1.charAt(i)-'0';
-                i++;
-            }
-            ls1.add(version);
-            version=0;
-        }
-        
-        for(int i=0;i<version2.length();){
-            
-            if(version2.charAt(i)=='.'){
-                i++;
-                continue;
-            }
-            
-            int version=0;
-            while(i<version2.length() && version2.charAt(i)!='.'){
-                version=version*10+version2.charAt(i)-'0';
-                i++;
-            }
-            ls2.add(version);
-            version=0;
-        }
-        int i=0;
-        for(i=0;i<ls1.size() && i<ls2.size() ;i++){
-            if(ls1.get(i)==ls2.get(i))
-                continue;
-            if(ls1.get(i)>ls2.get(i))
-                return 1;
-            else
-                return -1;
-        }
-        
-        while(i<ls1.size()){
-            if(ls1.get(i)>0)
-                return 1;
-            i++;
-        }
-        
-        while(i<ls2.size()){
-            if(ls2.get(i)>0)
-                return -1;
-            i++;
-        }
-        return 0;
+    String[] levels1 = version1.split("\\.");
+    String[] levels2 = version2.split("\\.");
+    
+    int length = Math.max(levels1.length, levels2.length);
+    for (int i=0; i<length; i++) {
+    	Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
+    	Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
+    	int compare = v1.compareTo(v2);
+    	if (compare != 0) {
+    		return compare;
+    	}
+    }
+    
+    return 0;
     }
 }
