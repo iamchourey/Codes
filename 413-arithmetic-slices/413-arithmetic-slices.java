768fbe106ae79f940c6c20764c9feb8318a96863
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-03-03 08:47:54 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (17.44%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -4,26 +4,18 @@ class Solution {
         if(nums.length<3)
             return 0;
         
-        int dp[]=new int[nums.length];
-        dp[0]=0;
-        dp[1]=0;
+        int ans=0,curr=0;
         
         for(int i=2;i<nums.length;i++){
-            if(isAP(nums[i-2],nums[i-1],nums[i]))
-                dp[i]=1+dp[i-1];
+            
+            if(nums[i-1]-nums[i-2] == nums[i]-nums[i-1]){
+                curr+=1;
+                ans+=curr;
+            }
+            else
+                curr=0;
         }
         
-        int ans=0;
-        for(int num:dp)
-            ans+=num;
-        
         return ans;
-    }
-    
-    public boolean isAP(int a,int b,int c){
-        if(b-a == c-b)
-            return true;
-        else
-            return false;
     }
 }
