2cf84e74aa4d1b12e32cabcc9f53c1390b3431ed
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-03-06 09:03:29 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (47.43%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,15 +1,14 @@
 class Solution {
     public int countOrders(int n) {
         
-        long[] dp=new long[n+1];
-        dp[0]=1;
+        long res=1;
         int mod=1000000007;
         
         for(int i=1;i<=n;i++){
-            dp[i]=i*dp[i-1]*(2*i-1);
-            dp[i]%=mod;
+            res=i*res*(2*i-1);
+            res%=mod;
         }
         
-        return (int)dp[n];
+        return (int)res;
     }
 }
