3ca59364a60d9f828f45c11334783645652a9d5f
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-03-13 10:11:23 +0530

    Time: 1 ms (83.78%), Space: 49.9 MB (56.01%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -3,18 +3,15 @@ class Solution {
         
         int m=mat.length,n=mat[0].length;
         if(m*n!=r*c) return mat;
-        List<Integer> ls=new ArrayList<>();
         int ans[][]=new int[r][c];
-        
+        int column=0,row=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                ls.add(mat[i][j]);
-            }
-        }
-        int index=0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                ans[i][j]=ls.get(index++);
+                ans[row][column++]=mat[i][j];
+                if(column==c){
+                    row++;
+                    column=0;
+                }
             }
         }
         return ans;
