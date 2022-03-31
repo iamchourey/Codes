f799f1a9e712cdd30a1bbf15f26708bcfd20c9c4
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-03-31 10:13:52 +0530

    Time: 1 ms (84.23%), Space: 41.8 MB (46.74%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.java
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -1,27 +1,27 @@
 class Solution {
     public int splitArray(int[] nums, int m) {
-        
         int max=Integer.MIN_VALUE,sum=0;
         for(int num:nums){
             max=Math.max(max,num);
             sum+=num;
         }
-        int start=max,end=sum;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(isFeasible(nums,m,mid)) end=mid-1;
-            else start=mid+1;
+        int left=max,right=sum;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(isFeasible(nums,m,mid)) right=mid-1;
+            else left=mid+1;
         }
-        return start;
+        return left;
     }
-    public boolean isFeasible(int nums[],int m,int ans){
-        int parts=1,sum=0;
+    public boolean isFeasible(int[] nums,int m,int res){
+        int sum=0,parts=1;
         for(int i=0;i<nums.length;i++){
-            if((sum+nums[i])>ans){
-                sum=nums[i];
+            if((sum+nums[i])<=res) sum+=nums[i];
+            else{
                 parts++;
-            }else sum+=nums[i];
+                sum=nums[i];
+            }
         }
-        return (parts<=m);
+        return parts<=m;
     }
 }
