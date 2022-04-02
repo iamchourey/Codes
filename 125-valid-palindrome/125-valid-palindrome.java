f3fd53a92b4eaf09c2c9b6dd4c6ad3c6800c4442
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-04-02 11:03:14 +0530

    Time: 4 ms (83.58%), Space: 43.6 MB (59.12%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,34 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str=s.trim();
-        int i=0,j=str.length()-1;
-        str=str.toLowerCase();
-        System.out.print(str);
         
-        while(i<j)
-        {
-            if((int)str.charAt(i)>=58 && (int)(str.charAt(i))<=64 ||
-               (int)str.charAt(i)>=33 && (int)(str.charAt(i))<=47 ||
-               (int)str.charAt(i)>=91 && (int)(str.charAt(i))<=96 ||
-               (int)str.charAt(i)>=123 && (int)(str.charAt(i))<=126 || str.charAt(i)==' '){
-                i++;
-                continue;
-            }
+        int start=0,end=s.length()-1;
+        
+        while(start<end){
+            while(start<end && !Character.isLetterOrDigit(s.charAt(start))) start++;
+            while(start<end && !Character.isLetterOrDigit(s.charAt(end))) end--;
             
-            if((int)str.charAt(j)>=58 && (int)(str.charAt(j))<=64 ||
-               (int)str.charAt(j)>=33 && (int)(str.charAt(j))<=47 ||
-               (int)str.charAt(j)>=91 && (int)(str.charAt(j))<=96 ||
-               (int)str.charAt(j)>=123 && (int)(str.charAt(j))<=126 || str.charAt(j)==' '){
-                j--;
-                continue;
-            }
-            if(str.charAt(i)!=str.charAt(j)) return false;
-            else{
-                ++i;
-                --j;
-            }
+            if(Character.toLowerCase(s.charAt(start))!=Character.toLowerCase(s.charAt(end)))
+                return false;
+            
+            start++;
+            end--;
         }
-        
         return true;
     }
 }
