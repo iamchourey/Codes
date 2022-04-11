db4b7b7b07b5edd3cb58a9712249393ba598ba15
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-04-11 07:36:53 +0530

    Time: 9 ms (52.71%), Space: 54.8 MB (39.15%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,39 +1,22 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int m=grid.length,n=grid[0].length;
-        int[] temp=new int[m*n];
-        int index=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                temp[index++]=grid[i][j];
-            }
-        }
-        k%=m*n;
-        reverse(temp,0,m*n-1);
-        reverse(temp,0,k-1);
-        reverse(temp,k,m*n-1);
         
         List<List<Integer>> ls=new ArrayList<>();
-        int row=0;
-        for(int i=0;i<m*n;){
-            ls.add(new ArrayList<>());
-            for(int j=0;j<n && i<m*n;j++){
-                ls.get(row).add(temp[i++]);
+        int m=grid.length,n=grid[0].length;
+        k%=(m*n);
+        int start=(m*n)-k;
+        int index=-1;
+        for(int i=start;i<m*n+start;i++){
+            int j=i%(m*n),row=j/n,col=j%n;
+            
+            if((i-start)%n==0){
+                ls.add(new ArrayList<>());
+                index++;
             }
-            row++;
+            
+            ls.get(index).add(grid[row][col]);
         }
         
         return ls;
-    }
-    
-    public void reverse(int[] temp,int start,int end){
-        while(start<end){
-            int a=temp[start];
-            temp[start]=temp[end];
-            temp[end]=a;
-            
-            start++;
-            end--;
-        }
     }
 }
