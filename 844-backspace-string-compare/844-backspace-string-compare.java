56bad1cec6846bd2a4a9db975f309aa4799c80f6
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-05-01 21:05:55 +0530

    Time: 1 ms (87.56%), Space: 42.3 MB (38.73%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,17 +1,24 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        String str1="",str2="";
         
-        for(char c:s.toCharArray()){
-            if(str1.length()>0 && c=='#') str1=str1.substring(0,str1.length()-1);
-            else if(c!='#') str1+=Character.toString(c);
+        int i=s.length()-1,j=t.length()-1,back;
+        
+        while(true){
+            back=0;
+            while(i>=0 && (back>0 || s.charAt(i)=='#')){
+                back+=s.charAt(i)=='#'?1:-1;
+                i--;
+            }
+            back=0;
+            while(j>=0 && (back>0 || t.charAt(j)=='#')){
+                back+=t.charAt(j)=='#'?1:-1;
+                j--;
+            }
+            if(i>=0 && j>=0 && s.charAt(i)==t.charAt(j)){
+                i--;
+                j--;
+            }else break;
         }
-        
-        for(char c:t.toCharArray()){
-            if(str2.length()>0 && c=='#') str2=str2.substring(0,str2.length()-1);
-            else if(c!='#') str2+=Character.toString(c);
-        }
-        
-        return str1.equals(str2);
+        return i==-1 && j==-1;
     }
 }
