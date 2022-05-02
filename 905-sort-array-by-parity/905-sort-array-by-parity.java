368b4abf79d6f8acd9cee838cacb0ba30f2387f4
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-05-02 10:31:52 +0530

    Time: 1 ms (96.61%), Space: 47.7 MB (71.07%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -1,13 +1,9 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        
-        int even=-1;
-        
-        for(int i=0;i<nums.length;i++){
+        for(int i=0,even=0;i<nums.length;i++){
             if(nums[i]%2==0){
-                even++;
                 int temp=nums[even];
-                nums[even]=nums[i];
+                nums[even++]=nums[i];
                 nums[i]=temp;
             }
         }
