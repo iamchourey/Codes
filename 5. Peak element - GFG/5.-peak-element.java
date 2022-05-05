d73d3dfd38aadeafcd062253f93ccc77c6c96579
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-05-05 10:03:10 +0530

    Added solution - LeetHub

---
+++ 5. Peak element - GFG/5.-peak-element.java
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+import java.util.*;
+class PeakElement{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			int[] a=new int[n];
+		    int[] tmp=new int[n];
+			for(int i=0;i<n;i++){
+				a[i]=sc.nextInt();
+				tmp[i] = a[i];
+			}
+		    
+		    int f=0;
+			int A=(new Solution()).peakElement(tmp,n);
+			
+			if(A<0 && A>=n)
+			    System.out.println(0);
+			else
+			{
+    			if(n==1 && A==0)
+    			    f=1;
+    			else if(A==0 && a[0]>=a[1])
+    			    f=1;
+    			else if(A==n-1 && a[n-1]>=a[n-2])
+    			    f=1;
+    			else if(A >= 0 && A < n && a[A] >=a[A+1] && a[A]>= a[A-1])
+    			    f=1;
+    			else
+    			    f=0;
+			    System.out.println(f);
+			}
+		}
+	}
+}// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    {
+       //add code here.
+       int start=0,end=n-1;
+       
+       while(start<end){
+           int mid=start+(end-start)/2;
+           if((mid==0 || arr[mid-1]<=arr[mid]) && (mid==n-1 || arr[mid+1]<=arr[mid]))
+           return mid;
+           else if(mid>0 && arr[mid]<=arr[mid-1]) end=mid-1;
+           else start=mid+1;
+       }
+       
+       return start;
+    }
+}
