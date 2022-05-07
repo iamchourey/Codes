ddb348930df5bbef646f5ba8c8f0d1e1cbfffdce
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-05-07 20:08:41 +0530

    Added solution - LeetHub

---
+++ 7. Mother Vertex - GFG/7.-mother-vertex.java
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+            for(int i = 0; i < V; i++)
+                adj.add(i, new ArrayList<Integer>());
+            for(int i = 0; i < E; i++){
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+            }
+            Solution obj = new Solution();
+            int ans = obj.findMotherVertex(V, adj);
+            System.out.println(ans);
+       }
+    }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to find a Mother Vertex in the Graph.
+    public int findMotherVertex(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        // Code here
+        int[] nodes=new int[]{V};
+        for(int i=0;i<V;i++){
+            dfs(adj,nodes,i,new boolean[V]);
+            if(nodes[0]==0) return i;
+            nodes[0]=V;
+        }
+        return -1;
+    }
+    
+    public static void dfs(ArrayList<ArrayList<Integer>>adj,int[] nodes,int src,boolean[] visited){
+        visited[src]=true;
+        nodes[0]--;
+        
+        ArrayList<Integer> neighbors=adj.get(src);
+        
+        for(int neighbor:neighbors){
+            if(visited[neighbor]==false){
+                dfs(adj,nodes,neighbor,visited);
+            }
+        }
+    }
+}
