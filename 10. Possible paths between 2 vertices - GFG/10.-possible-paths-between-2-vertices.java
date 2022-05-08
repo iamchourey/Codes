68193eb5feab9077d96230155e0733351ca50506
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-05-08 12:09:34 +0530

    Added solution - LeetHub

---
+++ 10. Possible paths between 2 vertices - GFG/10.-possible-paths-between-2-vertices.java
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            String[] S1 = br.readLine().trim().split(" ");
+            int source = Integer.parseInt(S1[0]);
+            int destination = Integer.parseInt(S1[1]);
+            Solution obj = new Solution();
+            System.out.println(obj.countPaths(V, adj, source, destination));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to count paths between two vertices in a directed graph.
+    public int countPaths(int V, ArrayList<ArrayList<Integer>> adj, int source,
+                   int destination) {
+        // Code here
+        int res[]=new int[1];
+        dfs(adj,V,source,destination,res);
+        return res[0];
+    }
+    
+    public void dfs(ArrayList<ArrayList<Integer>> adj,int V,int src,int dest,int[] res){
+        if(src==dest){
+            res[0]++;
+            return;
+        }
+        
+        for(int i=0;i<adj.get(src).size();i++){
+            dfs(adj,V,adj.get(src).get(i),dest,res);
+        }
+    }
+}
