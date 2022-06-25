f4406fdc2afd6d36a6af16cb6830c9ee4e399daa
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-06-25 14:45:54 +0530

    Time: 24 ms (72.52%), Space: 46.1 MB (30.31%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,15 +1,58 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long res=0;
+        long mins=sumSubarrayMins(nums);
+        long maxs=sumSubarrayMaxs(nums);
         
-        for(int i=0;i<nums.length;i++){
-            int min=nums[i];
-            int max=nums[i];
-            for(int j=i+1;j<nums.length;j++){
-                max=Math.max(max,nums[j]);
-                min=Math.min(min,nums[j]);
-                res+=(max-min);
+        return maxs-mins;
+    }
+    
+    public long sumSubarrayMins(int[] arr) {
+        
+        Stack<Integer> stack=new Stack<>();
+        long res=0;
+ 
+        for(int i=0;i<arr.length;i++){
+            while(stack.isEmpty()==false && arr[stack.peek()]>=arr[i]){
+                int index=stack.pop();
+                int right=i-index-1;
+                int left=(stack.isEmpty()?index:index-stack.peek()-1);
+                long subarrays=(long)((right+1)*(left+1));
+                res+=(long)(arr[index]*subarrays);
             }
+            stack.push(i);
+        }
+        
+        while(stack.isEmpty()==false){
+            int index=stack.pop();
+            int right=arr.length-index-1;
+            int left=(stack.isEmpty()?index:index-stack.peek()-1);
+            long subarrays=(long)((right+1)*(left+1));
+            res+=(long)(arr[index]*subarrays);
+        }
+        return res;
+    }
+    
+     public long sumSubarrayMaxs(int[] arr) {
+        
+        Stack<Integer> stack=new Stack<>();
+        long res=0;
+        for(int i=0;i<arr.length;i++){
+            while(stack.isEmpty()==false && arr[stack.peek()]<=arr[i]){
+                int index=stack.pop();
+                int right=i-index-1;
+                int left=(stack.isEmpty()?index:index-stack.peek()-1);
+                long subarrays=(long)((right+1)*(left+1));
+                res+=(long)(arr[index]*subarrays);
+            }
+            stack.push(i);
+        }
+        
+        while(stack.isEmpty()==false){
+            int index=stack.pop();
+            int right=arr.length-index-1;
+            int left=(stack.isEmpty()?index:index-stack.peek()-1);
+            long subarrays=(long)((right+1)*(left+1));
+            res+=(long)(arr[index]*subarrays);
         }
         return res;
     }
