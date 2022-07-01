1ab1a7240fb5025823af12e39894c55185360351
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-07-01 09:19:19 +0530

    Time: 119 ms (7.30%), Space: 46.5 MB (8.86%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,24 +1,61 @@
 class Solution {
+    int a=-1,b=-1;
     public int minMoves2(int[] nums) {
-        Arrays.sort(nums);
+        int median = getMedian(nums,nums.length);
+        
         int res=0;
-        int n=nums.length;
-        
-        if(n%2==0){
-            int res1=helper(nums,nums[n/2]);
-            int res2=helper(nums,nums[n/2 - 1]);
-            res=Math.min(res1,res2);
+        for(int num:nums){
+            res+=Math.abs(median-num);
         }
-        else res=helper(nums,nums[n/2]);
-        
         return res;
     }
     
-    public int helper(int[] nums,int val){
-        int res=0;
-        for(int num:nums){
-            res+=Math.abs(val-num);
+    public int getMedian(int[] nums,int n){
+        int ans=0;
+        if(n%2==0){
+            median(nums,0,n-1,n/2);
+            return (a+b)/2;
         }
-        return res;
+        else {
+            median(nums,0,n-1,n/2);
+            return b;
+        }
+    }
+    public void median(int []nums,int start,int end,int k){
+        if(start<=end){
+            int p=partition(nums,start,end);
+            if(p==k){
+                b=nums[p];
+                if(a!=-1) return;
+            }
+            else if(p==k-1){
+                a=nums[p];
+                if(b!=-1) return;
+            }
+
+            if(p>=k){
+                median(nums,start,p-1,k);
+            }
+            else median(nums,p+1,end,k);
+        }
+    }
+    
+    public int partition(int[] nums,int l,int r){
+        int pivot=nums[r];
+        int index=l-1;
+        
+        for(int i=l;i<r;i++){
+            if(nums[i]<=pivot){
+                index++;
+                swap(nums,i,index);
+            }
+        }
+        swap(nums,++index,r);
+        return index;
+    }
+    public void swap(int nums[],int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
