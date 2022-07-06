7c604f982da74cd5c7821c3983a8e9509aaf9f25
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-07-06 08:17:14 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (49.33%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
+        int prev1=0,prev2=1,res=1;
+        
+        for(int i=2;i<=n;i++){
+            res=prev1+prev2;
+            prev1=prev2;
+            prev2=res;
+        }
+        return res;
+    }
+}
