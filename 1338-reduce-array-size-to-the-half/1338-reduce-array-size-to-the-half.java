027fd61786022594defdd186f529a0d32cc8fadf
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-08-18 15:40:22 +0530

    Time: 88 ms (59.26%), Space: 86.6 MB (55.14%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,34 +1,25 @@
 class Solution {
-    class Pair{
-        int num,occurances;
-        
-        public Pair(int num,int occ){
-            this.num=num;
-            this.occurances=occ;
-        }
-    }
     public int minSetSize(int[] arr) {
         
         Map<Integer,Integer> map=new HashMap<>();
         int size=arr.length;
+        
         for(int num:arr){
             map.put(num,map.getOrDefault(num,0)+1);
         }
         
-        PriorityQueue<Pair> pq=new PriorityQueue<>(new Comparator<Pair>(){
-            public int compare(Pair p1,Pair p2){
-                return p2.occurances-p1.occurances;
-            }
-        });
+        int temp[]=new int[map.size()];
+        int index=0,result=0;
         
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            pq.add(new Pair(entry.getKey(),entry.getValue()));
+        for(int key:map.keySet()){
+            temp[index++]=map.get(key);
         }
         
-        int result=0;
+        Arrays.sort(temp);
+        index=temp.length-1;
         
         while(size>arr.length/2){
-            size-=pq.poll().occurances;
+            size-=temp[index--];
             result++;
         }
         
