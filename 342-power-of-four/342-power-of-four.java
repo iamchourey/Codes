31f13596888c134edc28cf7d73b15e52848996ed
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-08-22 10:06:54 +0530

    Time: 1 ms (100.00%), Space: 42.5 MB (5.32%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,11 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        
-        if(n==0)
-            return false;
-        if(n==1)
-            return true;
-        
-        return (n>0 && (n&(n-1))==0 && (n&(0x55555555))!=0);
+        if(n==1) return true;
+        else if(n<4) return false;
+        else return n%4==0 && isPowerOfFour(n/4);
     }
 }
