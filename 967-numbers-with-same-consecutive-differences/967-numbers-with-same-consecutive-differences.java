897599a2f0aeee353955dd5070c74bd82e5ae971
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-09-03 11:12:42 +0530

    Time: 4 ms (65.31%), Space: 43.7 MB (20.31%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -3,9 +3,10 @@ class Solution {
         
         List<Integer> res=new ArrayList<>();
         List<Integer> temp=new ArrayList<>();
+        Set<Integer> set=new HashSet<>();
         for(int i=1;i<=9;i++){
             temp.add(i);
-            generateNumbers(n-1,k,i,temp,res);
+            generateNumbers(n-1,k,i,temp,res,set);
             temp.remove(0);
         }
         
@@ -16,25 +17,28 @@ class Solution {
         return result;
     }
     
-    public void generateNumbers(int n,int k,int lastDigit,List<Integer> temp,List<Integer> res){
+    public void generateNumbers(int n,int k,int lastDigit,List<Integer> temp,List<Integer> res,Set<Integer> set){
         if(n==0){
             int num=0,pow=1;
             for(int i=temp.size()-1;i>=0;i--){
                 num+=temp.get(i)*pow;
                 pow*=10;
             }
-            if(!res.contains(num)) res.add(num);
+            if(!set.contains(num)){
+                res.add(num);
+                set.add(num);
+            }
             return;
         }
         
         if(lastDigit+k<10){
             temp.add(lastDigit+k);
-            generateNumbers(n-1,k,lastDigit+k,temp,res);
+            generateNumbers(n-1,k,lastDigit+k,temp,res,set);
             temp.remove(temp.size()-1);
         }
         if(lastDigit-k>=0){
             temp.add(lastDigit-k);
-            generateNumbers(n-1,k,lastDigit-k,temp,res);
+            generateNumbers(n-1,k,lastDigit-k,temp,res,set);
             temp.remove(temp.size()-1);
             
         }
