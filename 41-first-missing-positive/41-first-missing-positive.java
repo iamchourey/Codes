7e9b44eb3a8b25b4b27fdb365cbd33bca491b100
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-09-22 23:24:45 +0530

    Time: 3 ms (70.37%), Space: 57.9 MB (64.95%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -3,19 +3,18 @@ class Solution {
         
         int n=nums.length;
         boolean containsN=false;
-        Set<Integer> set=new HashSet<>();
+        
         for(int i=0;i<n;i++){
             if(nums[i]==n) containsN=true;
-            if(nums[i]<0) nums[i]=n;
+            if(nums[i]<=0) nums[i]=n;
         }
         for(int i=0;i<n;i++){
-            if(Math.abs(nums[i])<n && !set.contains(Math.abs(nums[i]))){
+            if(Math.abs(nums[i])<n && nums[Math.abs(nums[i])]>0){
                 nums[Math.abs(nums[i])]*=-1;
-                set.add(Math.abs(nums[i]));
             }
         }
         for(int i=1;i<n;i++){
-            if(nums[i]>=0 && !set.contains(i)) return i;
+            if(nums[i]>0) return i;
         }
         return containsN?n+1:n;
     }
