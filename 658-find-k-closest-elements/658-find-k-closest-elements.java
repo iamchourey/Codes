c6b8177a51dd2e743e84f642dd2f349e49b3bfb5
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-09-29 10:09:17 +0530

    Time: 41 ms (25.69%), Space: 62.2 MB (70.94%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,26 +1,28 @@
 class Solution {
-    class Pair{
-        int diff,val;
-        public Pair(int diff,int val){
-            this.diff=diff;
-            this.val=val;
-        }
-    }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         
         List<Integer> result=new ArrayList<>();
-        PriorityQueue<Pair> pq=new PriorityQueue<>(new Comparator<Pair>(){
-            public int compare(Pair p1,Pair p2){
-                if(p2.diff==p1.diff) return p2.val-p1.val;
-                return p2.diff-p1.diff;
-            }
-        });
+        int start=0,end=arr.length-1;
         
-        for(int num:arr){
-            pq.add(new Pair(Math.abs(x-num),num));
-            if(pq.size()>k) pq.poll();
+        while(end-start>1){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>=x) end=mid;
+            else start=mid;
         }
-        while(pq.isEmpty()==false) result.add(pq.poll().val);
+        
+        while(result.size()<k){
+            int leftVal=(start>=0)?arr[start]:Integer.MAX_VALUE/2;
+            int rightVal=(end<arr.length)?arr[end]:Integer.MAX_VALUE/2;
+            
+            if(Math.abs(x-leftVal)<=Math.abs(rightVal-x)){
+                result.add(leftVal);
+                start--;
+            }
+            else{
+                result.add(rightVal);
+                end++;
+            }
+        }
         Collections.sort(result);
         return result;
     }
