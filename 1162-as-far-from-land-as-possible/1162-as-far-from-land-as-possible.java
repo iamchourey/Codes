e6819cfd451885d0aae099db333fba96bba298e2
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-10-25 20:32:08 +0530

    Time: 41 ms (35.44%), Space: 64.8 MB (43.28%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,18 +1,17 @@
 class Solution {
     public int maxDistance(int[][] grid) {
-        int m=grid.length,n=grid[0].length;
-        int lands=0;
+        int m=grid.length,n=grid[0].length,lands=0;
         Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    lands++;
                     q.add(new int[]{i,j});
+                    lands++;
                 }
             }
         }
-        if(lands==m*n) return -1;
-        int[][] dirs=new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+        if(lands==0 || lands==m*n) return -1;
+        int[][] dirs={{-1,0},{1,0},{0,-1},{0,1}};
         int distance=-1;
         while(q.isEmpty()==false){
             int size=q.size();
@@ -20,12 +19,11 @@ class Solution {
             while(size-->0){
                 int[] curr=q.poll();
                 for(int[] dir:dirs){
-                    int x=curr[0]+dir[0];
-                    int y=curr[1]+dir[1];
-                    
+                    int x=dir[0]+curr[0];
+                    int y=dir[1]+curr[1];
                     if(x>=0 && y>=0 && x<m && y<n && grid[x][y]==0){
-                        grid[x][y]=1;
                         q.add(new int[]{x,y});
+                        grid[x][y]=1;
                     }
                 }
             }
