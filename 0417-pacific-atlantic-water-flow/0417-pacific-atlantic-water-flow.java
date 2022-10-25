22fe123c03fcf012c8f0d4343b7781e3686097f7
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-10-25 11:23:55 +0530

    Time: 77 ms (11.11%), Space: 55 MB (43.48%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -3,12 +3,14 @@ class Solution {
         List<List<Integer>> res=new ArrayList<>();
         int m=heights.length,n=heights[0].length;
         boolean visited[][]=new boolean[m][n];
+        boolean dp[][]=new boolean[m][n];
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 boolean bothOceans[]=new boolean[2];
-                dfs(heights,m,n,i,j,bothOceans,visited);
+                dfs(heights,m,n,i,j,bothOceans,visited,dp);
                 if(bothOceans[0] && bothOceans[1]){
+                    dp[i][j]=true;
                     List<Integer> temp=new ArrayList<>();
                     temp.add(i);
                     temp.add(j);
@@ -18,15 +20,20 @@ class Solution {
         }
         return res;
     }
-    public void dfs(int[][] heights,int m,int n,int x,int y,boolean[] bothOceans,boolean[][] visited){
+    public void dfs(int[][] heights,int m,int n,int x,int y,boolean[] bothOceans,boolean[][] visited,boolean[][] dp){
+        if(dp[x][y]){
+            bothOceans[0]=true;
+            bothOceans[1]=true;
+            return;
+        }
         if(x<0 || y<0 || x>=m || y>=n || visited[x][y] || (bothOceans[0] && bothOceans[1])) return;
         if(x==0 || y==0) bothOceans[0]=true;
         if(x==m-1 || y==n-1) bothOceans[1]=true;
         visited[x][y]=true;
-        if(x>0 && heights[x-1][y]<=heights[x][y]) dfs(heights,m,n,x-1,y,bothOceans,visited);
-        if(y>0 && heights[x][y-1]<=heights[x][y]) dfs(heights,m,n,x,y-1,bothOceans,visited);
-        if(x<m-1 && heights[x+1][y]<=heights[x][y]) dfs(heights,m,n,x+1,y,bothOceans,visited);
-        if(y<n-1 && heights[x][y+1]<=heights[x][y]) dfs(heights,m,n,x,y+1,bothOceans,visited);
+        if(x>0 && heights[x-1][y]<=heights[x][y]) dfs(heights,m,n,x-1,y,bothOceans,visited,dp);
+        if(y>0 && heights[x][y-1]<=heights[x][y]) dfs(heights,m,n,x,y-1,bothOceans,visited,dp);
+        if(x<m-1 && heights[x+1][y]<=heights[x][y]) dfs(heights,m,n,x+1,y,bothOceans,visited,dp);
+        if(y<n-1 && heights[x][y+1]<=heights[x][y]) dfs(heights,m,n,x,y+1,bothOceans,visited,dp);
         visited[x][y]=false;
     }
 }
