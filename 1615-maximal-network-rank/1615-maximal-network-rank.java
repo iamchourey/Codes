0989411a6fb37a7909c3a4c05e14eb245093d32d
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-11-04 18:22:28 +0530

    Time: 19 ms (51.94%), Space: 53.4 MB (79.80%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,24 +1,21 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
         int []indegree=new int[n];
-        Set<String> set=new HashSet<>();
+        int[][] isEdge=new int[n][n];
         
         for(int[] road:roads){
             int u=road[0];
             int v=road[1];
             indegree[u]++;
             indegree[v]++;
-            String temp=Character.toString((char)(u+'0'))+Character.toString((char)(v+'0'));
-            set.add(temp);
+            isEdge[u][v]=1;
+            isEdge[v][u]=1;
         }
         int result=0;
         
         for(int u=0;u<n-1;u++){
             for(int v=u+1;v<n;v++){
-                int rank=indegree[u]+indegree[v];
-                String temp1=Character.toString((char)(u+'0'))+Character.toString((char)(v+'0'));
-                String temp2=Character.toString((char)(v+'0'))+Character.toString((char)(u+'0'));
-                if(set.contains(temp1) || set.contains(temp2)) rank-=1;
+                int rank=indegree[u]+indegree[v]-isEdge[u][v];
                 result=Math.max(result,rank);
             }
         }
