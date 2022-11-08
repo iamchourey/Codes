8cb152e64f53d2d61d43c2e009797c3545187839
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-11-08 15:59:20 +0530

    Time: 353 ms (49.69%), Space: 206.1 MB (6.59%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,38 +1,41 @@
 class Solution {
+    class Pair{
+        int[] arr;
+        int dist;
+        public Pair(int[] arr,int dist){
+            this.arr=arr;
+            this.dist=dist;
+        }
+    }
     public int minCostConnectPoints(int[][] points) {
-        
-        PriorityQueue<Pair<int[],Integer>> pq=new PriorityQueue<>(new Comparator<Pair>(){
+        int result=0;
+        PriorityQueue<Pair> pq=new PriorityQueue<>(new Comparator<Pair>(){
             public int compare(Pair p1,Pair p2){
-                return (int)p1.getValue()-(int)p2.getValue();
+                return p1.dist-p2.dist;
             }
         });
-        
-        Set<Integer> hs=new HashSet<>();
-        hs.add(0);
-        getDistances(points,0,pq,hs);
-        
-        int count=0,res=0;
+        Set<Integer> set=new HashSet<>();
+        set.add(0);
+        getDistances(points,0,pq,set);
+        int count=0;
         
         while(count!=points.length-1){
-            Pair<int[],Integer> p=pq.poll();
-            int[] temp=p.getKey();
-            int dist = p.getValue();
-            if(hs.contains(temp[1])) continue;
+            Pair p=pq.poll();
+            int dist=p.dist;
+            int[] temp=p.arr;
+            if(set.contains(temp[1])) continue;
+            result+=dist;
             count++;
-            res+=dist;
-            hs.add(temp[1]);
-            getDistances(points,temp[1],pq,hs);
+            set.add(temp[1]);
+            getDistances(points,temp[1],pq,set);
         }
-        return res;
+        return result;
     }
-    
-    public void getDistances(int[][] points,int index,PriorityQueue<Pair<int[],Integer>> pq,Set<Integer> hs){
-        
+    public void getDistances(int[][] points,int index,PriorityQueue<Pair> pq,Set<Integer> set){
         for(int i=0;i<points.length;i++){
-            if(i!=index && !hs.contains(i)){
-                int temp[]=new int[]{index,i};
-                int dist=Math.abs(points[i][0]-points[index][0])+Math.abs(points[i][1]-points[index][1]);
-                pq.add(new Pair(temp,dist));
+            if(i!=index && !set.contains(i)){
+                int dist=Math.abs(points[index][0]-points[i][0])+Math.abs(points[index][1]-points[i][1]);
+                pq.add(new Pair(new int[]{index,i},dist));
             }
         }
     }
