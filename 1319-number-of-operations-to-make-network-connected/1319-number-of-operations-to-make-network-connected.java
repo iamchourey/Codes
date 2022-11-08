e329d5436ed672e2086eeb9ecce6595a12ae861f
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-11-08 17:57:22 +0530

    Time: 10 ms (83.75%), Space: 63.7 MB (76.49%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,30 +1,37 @@
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        int cables=connections.length;
-        if(cables<(n-1)) return -1;
-        int networks=0;
-        boolean seen[]=new boolean[n];
-        List<List<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        if(connections.length<n-1) return -1;
+        int[] parent=new int[n];
+        int[] rank=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            rank[i]=1;
+        }
         for(int[] connection:connections){
             int u=connection[0];
             int v=connection[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+            if(find(u,parent)!=find(v,parent)) union(u,v,parent,rank);
         }
+        int result=0;
         for(int i=0;i<n;i++){
-            if(seen[i]==false){
-                dfs(adj,n,seen,i);
-                networks++;
-            }
+            if(parent[i]==i) result++;
         }
-        return networks-1;
+        return result-1;
     }
-    public void dfs(List<List<Integer>> adj,int n,boolean[] seen,int src){
-        if(seen[src]) return;
-        seen[src]=true;
-        for(int node:adj.get(src)){
-            dfs(adj,n,seen,node);
+    public int find(int val,int[] parent){
+        if(parent[val]==val) return val;
+        return parent[val]=find(parent[val],parent);
+    }
+    public void union(int u,int v,int[] parent,int[] rank){
+        int leaderu=find(u,parent);
+        int leaderv=find(v,parent);
+        if(leaderu==leaderv) return;
+        if(rank[leaderu]>rank[leaderv]) parent[leaderv]=leaderu;
+        else if(rank[leaderv]>rank[leaderu]) parent[leaderu]=leaderv;
+        else{
+            parent[leaderv]=leaderu;
+            rank[leaderu]++;
         }
+        
     }
 }
