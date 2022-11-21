0b1e547b72ed2887698e59580fbf5c339a42cd4a
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-11-21 10:54:24 +0530

    Time: 25 ms (33.58%), Space: 71.4 MB (53.94%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,21 +1,21 @@
 class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
         int result=0,m=maze.length,n=maze[0].length;
-        Queue<int[]> q=new LinkedList<>();
-        q.add(entrance);
+        Queue<int[]> queue=new LinkedList<>();
+        queue.add(entrance);
         maze[entrance[0]][entrance[1]]='+';
-        int[][] dirs={{-1,0},{1,0},{0,1},{0,-1}};
-        while(q.isEmpty()==false){
-            int size=q.size();
+        int dirs[][]={{1,0},{-1,0},{0,1},{0,-1}};
+        while(queue.isEmpty()==false){
+            int size=queue.size();
             while(size-->0){
-                int[] curr=q.poll();
+                int curr[]=queue.poll();
                 if((curr[0]!=entrance[0] || curr[1]!=entrance[1]) && 
-                  (curr[0]==0 || curr[1]==0 || curr[0]==m-1 || curr[1]==n-1)) return result;
-                for(int[] dir:dirs){
-                    int x=dir[0]+curr[0];
-                    int y=dir[1]+curr[1];
+                   (curr[0]==0 || curr[0]==m-1 || curr[1]==0 || curr[1]==n-1)) return result;
+                for(int dir[]:dirs){
+                    int x=curr[0]+dir[0];
+                    int y=curr[1]+dir[1];
                     if(x>=0 && y>=0 && x<m && y<n && maze[x][y]=='.'){
-                        q.add(new int[]{x,y});
+                        queue.add(new int[]{x,y});
                         maze[x][y]='+';
                     }
                 }
