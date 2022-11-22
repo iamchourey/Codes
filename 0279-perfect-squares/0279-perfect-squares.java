88efccc7443a9b9935d79a5bf38bdce4633471df
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-11-22 08:09:43 +0530

    Time: 59 ms (54.63%), Space: 42.6 MB (78.73%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numSquares(int n) {
+        if(n<4) return n;
+        int[] dp=new int[n+1];
+        for(int i=0;i<=n;i++){
+            dp[i]=i;
+            for(int j=1;j*j<=i;j++){
+                dp[i]=Math.min(dp[i],1+dp[i-j*j]);
+            }
+        }
+        return dp[n];
+    }
+}
