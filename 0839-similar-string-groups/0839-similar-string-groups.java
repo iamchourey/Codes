b351fa3f91ea784febad532659ef2cdc849d2c1c
iamchourey <48974587+iamchourey@users.noreply.github.com>
2022-12-02 22:40:57 +0530

    Time: 23 ms (72.51%), Space: 42.9 MB (77.21%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.java
+++ 0839-similar-string-groups/0839-similar-string-groups.java
@@ -38,6 +38,7 @@ class Solution {
         int count=0;
         for(int i=0;i<s1.length();i++){
             if(s1.charAt(i)!=s2.charAt(i)) count++;
+            if(count>2) return false;
         }
         return count==0 || count==2;
     }
