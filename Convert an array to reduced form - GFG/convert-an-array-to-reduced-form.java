96736147f6dc2c538b0a413218e282b5dedbfee4
iamchourey <48974587+iamchourey@users.noreply.github.com>
2023-01-24 15:25:13 +0530

    Added solution - LeetHub

---
+++ Convert an array to reduced form - GFG/convert-an-array-to-reduced-form.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().convert(arr, n);
+            for (int i = 0; i < n; i++) {
+                out.print(arr[i] + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void convert(int[] arr, int n) {
+        //Code here
+        List<int[]> list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(new int[]{arr[i],i});
+        }
+        Collections.sort(list,(a,b)->(a[0]-b[0]));
+        for(int i=0;i<n;i++){
+            int index=list.get(i)[1];
+            arr[index]=i;
+        }
+    }
+}
