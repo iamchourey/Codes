a6f504e80d4c8aa2e28c38b61a16b28f0694352f
iamchourey <48974587+iamchourey@users.noreply.github.com>
2023-03-30 13:48:55 +0530

    Added solution - LeetHub

---
+++ Minimum Integer - GFG/minimum-integer.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.minimumInteger(N, A);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int minimumInteger(int N, int[] A) {
+        // code here
+        long sum=0;
+        int res=-1;
+        for(int a:A) sum+=(long)a;
+        for(int a:A){
+            if(sum<=((long)a*(long)N)){
+                if(res==-1) res=a;
+                else res=Math.min(res,a);
+            }
+        }
+        return res;
+    }
+}
+        
